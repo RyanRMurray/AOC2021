c49ff1e37a0db74741f5c8c30270cdda81c9e662
RyanRMurray <RyanRMurray@Hotmail.co.uk>
2021-11-30 21:19:20 +0000

    Rejigged answer recorder to get string rep of results. moved solutions array to solutions/mod.rs.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,46 @@
+use std::env;
+use std::fs;
+use std::io;
+mod solutions;
 mod utils;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut day_arg = String::new();
+    let input_arg;
 
-    let mut x = utils::Answer::default();
+    //get day number if none provided
+    match args.get(1) {
+        None => {
+            println!("Enter day number: ");
+            io::stdin()
+                .read_line(&mut day_arg)
+                .expect("Failed to read day number.");
+        }
+        Some(a) => day_arg = a.to_string(),
+    }
 
-    x.record(&"test");
+    let day: usize = day_arg.trim().parse().expect("Invalid day number format.");
 
-    x.record(&444);
+    if (day < 1) || (day > 25) {
+        println!("Not a valid day >:[");
+        return;
+    }
 
-    print!("{}",x);
+    //get input file
+    match args.get(2) {
+        Some(a) => input_arg = a.to_string(),
+        None => input_arg = "./input.txt".to_string(),
+    }
+
+    let input = fs::read_to_string(input_arg).expect("Could not open input file.");
+
+    //get solution if one exists
+    match solutions::DAYS.get(day - 1) {
+        None => println!("No solution for that day yet."),
+        Some(sol) => {
+            let a = sol(input);
+            println!("===========Day {}===========\n{}", day, a)
+        }
+    }
 }
--- src/utils.rs
+++ src/utils.rs
@@ -1,58 +1,60 @@
-use std::time::{Instant};
 use std::fmt::{Display, Formatter, Result};
+use std::time::Instant;
 
-type Record<'a> = (&'a dyn Display, Instant);
+pub type Solution = fn(String) -> Answer; // Solution functions
 
-pub struct Answer<'a>{
+type Record = (String, Instant); // Recording an answer and its timestamp
+
+pub struct Answer {
     start: Instant,
-    part1: Option<Record<'a>>,
-    part2: Option<Record<'a>>
+    part1: Option<Record>,
+    part2: Option<Record>,
 }
 
-impl Default for Answer<'_>{
-    fn default() -> Self{
+impl Default for Answer {
+    fn default() -> Self {
         Self {
             start: Instant::now(),
             part1: None,
-            part2: None
+            part2: None,
         }
     }
 }
 
-impl Display for Answer<'_>{
-    fn fmt(&self, f: &mut Formatter<'_>) -> Result{
-
-        match self.part1 {
+impl Display for Answer {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match &self.part1 {
             None => write!(f, "Part 1 is not yet implemented\n"),
-            Some((res1,t1)) => {
+            Some((res1, t1)) => {
                 let d1 = t1.saturating_duration_since(self.start);
                 write!(f, "Part 1 Result: {}\nPart 1 Runtime: {:?}\n", res1, d1)
-                .expect("Error trying to print Part 1 Results");
+                    .expect("Error trying to print Part 1 Results");
 
-                match self.part2{
+                match &self.part2 {
                     None => write!(f, "Part 2 is not yet implemented\n"),
-                    Some((res2,t2)) => {
-                        let d2 = t2.saturating_duration_since(t1);
+                    Some((res2, t2)) => {
+                        let d2 = t2.saturating_duration_since(*t1);
                         let d3 = t2.saturating_duration_since(self.start);
-                        write!(f, "Part 2 Result: {}\nPart 2 Runtime:  {:?}\nOverall Runtime: {:?}\n", res2, d2, d3)
+                        write!(
+                            f,
+                            "Part 2 Result: {}\nPart 2 Runtime:  {:?}\nOverall Runtime: {:?}\n",
+                            res2, d2, d3
+                        )
                     }
                 }
-
             }
         }
     }
 }
 
-impl<'a> Answer<'a>{
-    pub fn record(&mut self, res: &'a dyn Display){
-        match self.part1{
-            None => self.part1 = Some((res, Instant::now())),
-            Some(_) => {
-                match self.part2{
-                    None => self.part2 = Some((res, Instant::now())),
-                    Some(_) => panic!("Cannot write third part to an answer!")
-                }
-            }
+impl<'a> Answer {
+    pub fn record(&mut self, res: &'a dyn Display) {
+        match self.part1 {
+            None => self.part1 = Some((res.to_string(), Instant::now())),
+            Some(_) => match self.part2 {
+                None => self.part2 = Some((res.to_string(), Instant::now())),
+                Some(_) => panic!("Cannot write third part to an answer!"),
+            },
         }
     }
 }
