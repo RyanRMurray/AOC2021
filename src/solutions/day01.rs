f02f5caf04bf0f5207c51cc5b2acff9e23e402c6
RyanRMurray <RyanRMurray@Hotmail.co.uk>
2021-11-30 21:20:02 +0000

    Adding solutions folder, day01 placeholder

---
+++ src/solutions/day01.rs
@@ -0,0 +1,11 @@
+use crate::utils::Answer;
+
+pub fn day01(input: String) -> Answer {
+    let mut answer = Answer::default();
+
+    answer.record(&input);
+
+    answer.record(&input);
+
+    return answer;
+}
---
+++ src/solutions/mod.rs
@@ -0,0 +1,4 @@
+use crate::utils::Solution;
+pub mod day01;
+
+pub const DAYS: [Solution; 1] = [day01::day01];
