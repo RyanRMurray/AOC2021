b0a490e5d2eb23a95ff931c0f98b365920a25614
RyanRMurray <RyanRMurray@Hotmail.co.uk>
2021-12-04 09:57:48 +0000

    End of Day 04. Parsing could do with work

--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -2,5 +2,6 @@ use crate::utils::Solution;
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
-pub const DAYS: [Solution; 3] = [day01::day01, day02::day02, day03::day03];
+pub const DAYS: [Solution; 4] = [day01::day01, day02::day02, day03::day03, day04::day04];
