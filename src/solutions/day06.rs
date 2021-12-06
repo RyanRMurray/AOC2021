b47227f38e70684b039993c30df4a8bce04a7ab8
RyanRMurray <RyanRMurray@Hotmail.co.uk>
2021-12-06 09:04:04 +0000

    End of Day 06

---
+++ src/solutions/day06.rs
@@ -0,0 +1,41 @@
+use crate::utils::Answer;
+use std::collections::VecDeque;
+
+fn day_passes<'a>(mut population: VecDeque<usize>) -> VecDeque<usize> {
+    //we get the population of day 0 fish, which would be the first in the list
+    //we can then 'reset' them by adding them to day 6 fish, then appending their spawn
+    //at the end of our queue
+    let x = population.pop_front().unwrap();
+    population[6] += x;
+    population.push_back(x);
+
+    population
+}
+
+pub fn day06(input: String) -> Answer {
+    let mut answer = Answer::default();
+
+    //parse input into populations by age
+    let nums: Vec<u8> = input.split(",").map(|x| x.parse().unwrap()).collect();
+    let mut pops: VecDeque<usize> = (0..9)
+        .map(|v| nums.iter().filter(|x| **x == v).count())
+        .collect();
+
+    answer.record_parsed();
+
+    //part 1: proliferation for 80 days
+    for _ in 0..80 {
+        pops = day_passes(pops)
+    }
+
+    answer.record(&pops.iter().sum::<usize>());
+
+    //part 2: further proliferation
+    for _ in 0..(256-80){
+        pops = day_passes(pops)
+    }
+
+    answer.record(&pops.iter().sum::<usize>());
+    
+    return answer;
+}
--- src/solutions/mod.rs
+++ src/solutions/mod.rs
@@ -4,11 +4,13 @@ mod day02;
 mod day03;
 mod day04;
 mod day05;
+mod day06;
 
-pub const DAYS: [Solution; 5] = [
+pub const DAYS: [Solution; 6] = [
     day01::day01,
     day02::day02,
     day03::day03,
     day04::day04,
     day05::day05,
+    day06::day06,
 ];
